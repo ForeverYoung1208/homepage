bb54f11dba1dd9825477b5a0113404b4c60b8a0a
Igor <siafin2010@gmail.com>
2020-04-29 23:14:05 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import './css/styles.scss'
 
-const button = document.getElementById('card__test-button');
-button.addEventListener('click', testButtonClickHandler)
+// const button = document.getElementById('card__test-button');
+// button.addEventListener('click', testButtonClickHandler)
 
-async function testButtonClickHandler(e) {
-  e.preventDefault();
+// async function testButtonClickHandler(e) {
+//   e.preventDefault();
 
-  let gotUser = await fetch('http://127.0.0.1:3000/users.json').then(r=>r.json())
-  console.log('[gotUser]', gotUser);
+//   let gotUser = await fetch('http://justapi.fyoung.dp.ua/users.json').then(r=>r.json())
+//   console.log('[gotUser]', gotUser);
 
-  console.log('----now let\'s try POST' );
-  let u2 = {
-    user: {"email":"user2@gmail.com","name":"Named User2","age":"42","is_resident":true}
-  }
+//   console.log('----now let\'s try POST' );
+//   let u2 = {
+//     user: {"email":"user2@gmail.com","name":"Named User2","age":"42","is_resident":true}
+//   }
   
   
 
-  fetch('http://127.0.0.1:3000/users.json',{
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(u2) // body data type must match "Content-Type" header
-  }).then( r => console.log('[r]', r))
+//   fetch('http://justapi.fyoung.dp.ua/users.json',{
+//     method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(u2) // body data type must match "Content-Type" header
+//   }).then( r => console.log('[r]', r))
   
-}
+// }
